5122903b97f755bcadcb541ee20fb271607a1932
AnhDaoHoang <anhdh1@smartosc.com>
2022-07-12 08:58:00 +0700

    done1

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    let data = fs.readFileSync('./views/index.html').toString();
-    res.send(data);
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 
